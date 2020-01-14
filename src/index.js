3055adf1038e71a1abea1e4cbe5603904eab51bf
Russell Carter <russell.c12@gmail.com>
2020-01-13 22:09:09 -0500

    MrRobot font

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './MRROBOT.ttf'
+import './MrRobot.ttf'
 import './MrRobot.eot'
 import './MrRobot.svg'
 import './MrRobot.woff'
