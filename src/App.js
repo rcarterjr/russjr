bc23fff723a6f4146c5a88e95a30e7fa994b5dd1
Russell Carter <russell.c12@gmail.com>
2020-01-17 18:44:41 -0500

    components and resume

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import './App.css'
-import Typing from 'react-typing-animation'
 import Socials from './components/socials'
+import Hello from './components/hellofriend'
 
 function App() {
     return (
         <div className="main">
-            <Typing>
-                <span>Hello friend</span>
-                <Typing.Delay ms={3000} />
-                <Typing.Backspace count={15} />
-                <span>Welcome to RussJr.io</span>
-            </Typing>
+            < Hello />
             <Socials />
         </div>
     )
