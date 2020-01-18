8e9c9a5dc3789d87f5f3addb3f0542921cdbe459
Russell Carter <russell.c12@gmail.com>
2020-01-17 20:01:29 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react'
 import './App.css'
 import Socials from './components/socials'
 import Hello from './components/hellofriend'
+import Navbar from './components/navbar'
 
 function App() {
-    return (
-        <div className="main">
-            < Hello />
-            <Socials />
-        </div>
-    )
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <Hello />
+        <Socials />
+      </div>
+    </div>
+  )
 }
 
 export default App
