8be8fc4f701da2c8aa02ded7fda85161f219c072
Russell Carter <russell.c12@gmail.com>
2020-01-17 21:08:54 -0500

    move hook into function

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import '../App.css'
 import NavItem from './navitem'
 
-const [page, setPage] = useState('/')
 
 // make "HOME" path/link take the place of 
 // the page that is currently being viewed
 // no one currently links to homepage App
 
 function Nav() {
+    const [page, setPage] = useState('/')
   return (
     <div className="navbar">
         < NavItem item="About me" link="/aboutme" />
