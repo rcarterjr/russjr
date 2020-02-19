48e96155e2ffb096ee5b51377d35acbb146f5015
Russell Carter <russell.c12@gmail.com>
2020-02-19 09:29:11 -0500

    update twitter handle

--- src/components/socials.js
+++ src/components/socials.js
@@ -5,37 +5,37 @@ import twitter from '../icons/twitter.png'
 import github from '../icons/github.png'
 
 function Socials() {
-    return (
-        <div className="socials">
-            <a
-                href="https://linkedin.com/in/russellcarterjr"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <div className="img">
-                    <img src={linkedin} alt="linkedin" id="linkedin" />
-                </div>
-            </a>
-            <a
-                href="https://twitter.com/rcarterjr_"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <div className="img">
-                    <img src={twitter} alt="twitter" id="twitter" />
-                </div>
-            </a>
-            <a
-                href="https://github.com/rcarterjr"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <div className="img">
-                    <img src={github} alt="github" id="github" />
-                </div>
-            </a>
+  return (
+    <div className="socials">
+      <a
+        href="https://linkedin.com/in/russellcarterjr"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="img">
+          <img src={linkedin} alt="linkedin" id="linkedin" />
         </div>
-    )
+      </a>
+      <a
+        href="https://twitter.com/rvssjr"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="img">
+          <img src={twitter} alt="twitter" id="twitter" />
+        </div>
+      </a>
+      <a
+        href="https://github.com/rcarterjr"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="img">
+          <img src={github} alt="github" id="github" />
+        </div>
+      </a>
+    </div>
+  )
 }
 
 export default Socials
